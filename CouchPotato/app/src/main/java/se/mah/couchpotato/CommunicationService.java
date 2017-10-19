78031d14a48693074d924e34e11b9517b63a9dc8
Robin <robin.lennart.johnsson@gmail.com>
2017-10-19 11:38:57 +0200

    Started working on communicationService

---
+++ CouchPotato/app/src/main/java/se/mah/couchpotato/CommunicationService.java
@@ -0,0 +1,79 @@
+package se.mah.couchpotato;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Binder;
+import android.os.IBinder;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * @author Robin Johnsson & Jonatan Fridsten
+ */
+
+public class CommunicationService extends Service {
+
+    private RunOnThread thread;
+    private Buffer<String> buffer;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        thread = new RunOnThread();
+        buffer = new Buffer();
+        return Service.START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new LocalService();
+    }
+
+    public void run(){
+        thread.start();
+    }
+
+    public class LocalService extends Binder {
+
+        public CommunicationService getService(){
+            return CommunicationService.this;
+        }
+
+    }
+
+    public void sendToURL(String url){
+
+    }
+
+    private class BackgroundTask extends AsyncTask<String,String,String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            String response = "";
+            URL url;
+            try {
+                url = new URL("http://api.tvmaze.com/singlesearch/shows?q=girls");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                InputStream inStream = new BufferedInputStream(connection.getInputStream());
+                response = inStream.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+        }
+    }
+
+}
