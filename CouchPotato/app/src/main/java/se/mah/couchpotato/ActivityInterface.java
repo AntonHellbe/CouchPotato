5c1227ce135dcf9d5a09d72049fc18fe451a2ac2
Gustaf Bohlin <gustaf.t.bohlin@gmail.com>
2017-10-19 10:13:06 +0200

    edited interface file

--- CouchPotato/app/src/main/java/se/mah/couchpotato/ActivityInterface.java
+++ CouchPotato/app/src/main/java/se/mah/couchpotato/ActivityInterface.java
@@ -1,9 +1,12 @@
 package se.mah.couchpotato;
 
+import android.app.Fragment;
+
 /**
  * Created by Gustaf Bohlin on 19/10/2017.
  */
 
 public interface ActivityInterface {
-    
+
+    public Fragment getFragmentByTag(String TAG);
 }
