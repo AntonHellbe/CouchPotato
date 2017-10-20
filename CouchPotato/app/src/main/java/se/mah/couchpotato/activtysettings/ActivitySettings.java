67e81df62d76019b02f70b0c0d45ce1217a58f86
Jonatan Fridsten <fridstenjonatan@gmail.com>
2017-10-20 21:48:43 +0200

    Added settings fragment with an basic layout

---
+++ CouchPotato/app/src/main/java/se/mah/couchpotato/activtysettings/ActivitySettings.java
@@ -0,0 +1,15 @@
+package se.mah.couchpotato.activtysettings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import se.mah.couchpotato.R;
+
+public class ActivitySettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
