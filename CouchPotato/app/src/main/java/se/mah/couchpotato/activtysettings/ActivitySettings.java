8b835e319d7d665a78076ad864867b3c4b4155bd
Jonatan Fridsten <fridstenjonatan@gmail.com>
2017-10-23 20:24:56 +0200

    Fixed spinners in settings

--- CouchPotato/app/src/main/java/se/mah/couchpotato/activtysettings/ActivitySettings.java
+++ CouchPotato/app/src/main/java/se/mah/couchpotato/activtysettings/ActivitySettings.java
@@ -1,27 +1,78 @@
 package se.mah.couchpotato.activtysettings;
 
 import android.animation.Animator;
+import android.graphics.Color;
 import android.graphics.Point;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewAnimationUtils;
-import android.view.ViewGroup;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
-import android.widget.RelativeLayout;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ScrollView;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
 
 import se.mah.couchpotato.R;
 
 public class ActivitySettings extends AppCompatActivity {
+
+    private CheckBox checkBoxTheme;
+    private CheckBox checkBoxNsfw;
+    private Spinner spinnerCountry;
+    private Spinner spinnerLanguage;
+    private ArrayAdapter<CharSequence> adapterCountry;
+    private ArrayAdapter<CharSequence> adapterLanguage;
+    private Settings settings;
+    private String country,language;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        animateView(savedInstanceState);
+        initComp();
+    }
+
+
+    private void initComp() {
+        checkBoxTheme = (CheckBox) findViewById(R.id.checkBox_settings_theme);
+        checkBoxNsfw = (CheckBox) findViewById(R.id.checkBox_settings_nsfw);
+        spinnerCountry = (Spinner) findViewById(R.id.spinner_settings_countries);
+        spinnerLanguage = (Spinner) findViewById(R.id.spinner_settings_language);
+
+        adapterCountry = ArrayAdapter.createFromResource(this, R.array.settings_array_country, R.layout.spinner_settings);
+        spinnerCountry.setAdapter(adapterCountry);
+        spinnerCountry.getBackground().setColorFilter(Color.parseColor("#ffffff"), PorterDuff.Mode.SRC_ATOP);
+//        spinnerCountry.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                country = spinnerCountry.getSelectedItem().toString();
+//                Log.v("ActivitySettings","Selected country:" + country);
+//            }
+//        });
+
+        adapterLanguage = ArrayAdapter.createFromResource(this, R.array.settings_array_language, R.layout.spinner_settings);
+        spinnerLanguage.setAdapter(adapterLanguage);
+        spinnerLanguage.getBackground().setColorFilter(Color.parseColor("#ffffff"), PorterDuff.Mode.SRC_ATOP);
+//        spinnerLanguage.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                language = spinnerLanguage.getSelectedItem().toString();
+//                Log.v("ActivitySettings","Selected language:" + language);
+//            }
+//        });
+
+    }
+
+    private void animateView(Bundle savedInstanceState) {
+
         final int x = getIntent().getIntExtra("revealX", 0);
         final int y = getIntent().getIntExtra("revealY", 0);
         final int startRadius = getIntent().getIntExtra("startRadius", 0);
@@ -43,27 +94,5 @@ public class ActivitySettings extends AppCompatActivity {
             }
         });
 
-        /*v = findViewById(R.id.settings_view_animation);
-        Animation animation = new AlphaAnimation(1, 0);
-        animation.setStartOffset(200);
-        animation.setDuration(500);
-        v.startAnimation(animation);
-        animation.start();
-        animation.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                ((ViewGroup) v.getParent()).removeView(v);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });*/
     }
 }
