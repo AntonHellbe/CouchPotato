7a172923612726f72e1f2eb86100973058e9f801
Gustaf Bohlin <gustaf.t.bohlin@gmail.com>
2017-10-25 15:49:01 +0200

    added a custom spinner class to handle loops

---
+++ CouchPotato/app/src/main/java/se/mah/couchpotato/activtysettings/LanguageSpinner.java
@@ -0,0 +1,46 @@
+package se.mah.couchpotato.activtysettings;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.AttributeSet;
+import android.widget.Spinner;
+
+/**
+ * Created by Gustaf Bohlin on 25/10/2017.
+ */
+
+public class LanguageSpinner extends Spinner {
+    public LanguageSpinner(Context context) {
+        super(context);
+    }
+
+    public LanguageSpinner(Context context, int mode) {
+        super(context, mode);
+    }
+
+    public LanguageSpinner(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LanguageSpinner(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public LanguageSpinner(Context context, AttributeSet attrs, int defStyleAttr, int mode) {
+        super(context, attrs, defStyleAttr, mode);
+    }
+
+    public LanguageSpinner(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes, int mode) {
+        super(context, attrs, defStyleAttr, defStyleRes, mode);
+    }
+
+    public LanguageSpinner(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes, int mode, Resources.Theme popupTheme) {
+        super(context, attrs, defStyleAttr, defStyleRes, mode, popupTheme);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        //((ActivitySettings) getContext()).setSpinnerListener();
+    }
+}
