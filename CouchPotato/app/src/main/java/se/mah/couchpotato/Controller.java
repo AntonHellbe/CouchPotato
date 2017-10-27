60f60ea440a6fe4352033413e5f5dcff7a065606
Jonatan Fridsten <fridstenjonatan@gmail.com>
2017-10-27 17:15:27 +0200

    Sync

--- CouchPotato/app/src/main/java/se/mah/couchpotato/Controller.java
+++ CouchPotato/app/src/main/java/se/mah/couchpotato/Controller.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
